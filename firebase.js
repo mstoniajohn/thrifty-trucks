12b155908a156437a5345380b33a20c1d6c17503
Tonia Roganti <toniaroganti@gmail.com>
2022-09-21 16:51:46 -0400

    added firebase but need to add env vars for vercel

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCp9ggHHSKc0rzqM2PMBXtZHjvNlPT0fsU',
+	authDomain: 'thrifty-trucks.firebaseapp.com',
+	projectId: 'thrifty-trucks',
+	storageBucket: 'thrifty-trucks.appspot.com',
+	messagingSenderId: '218071105757',
+	appId: '1:218071105757:web:72d50cca5ff419ecfe9b34',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
