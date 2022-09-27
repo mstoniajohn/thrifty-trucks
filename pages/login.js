c3fef05d8957276a6249f89587719cae46e5d5bf
Tonia Roganti <toniaroganti@gmail.com>
2022-09-27 15:36:23 -0400

    variable not declared

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import Layout from '@components/layout/Layout';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Login = () => {
+	const router = useRouter();
 	const { currentUser } = useSelector((state) => state.user);
 	if (currentUser) {
 		router.push('/');
